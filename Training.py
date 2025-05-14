43464879b12045c9cf926ddb83867c546ce8a888
Theo Kempf <theokempf@protonmail.com>
2025-05-14 18:26:26 +0200

    training runs but log loss graph is weird

--- Training.py
+++ Training.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
-import json
+
 
 
 """
@@ -28,12 +28,23 @@ class Training():
         self.learning_rate = 0.00001
         
     def initialization(self):
+        """
+        W.shape = [input nb, output nb]
+        b.shape = [output nb, 1]
+        For W0 the input is the number of features in the training data.
+        For the other W the input is the number of neuron in the current layer.
+        """
         self.parameters = {}
         layers_nb = len(self.neuron_per_layer_list)
         np.random.seed(1)
-        for layer in range(1, layers_nb):
-            self.parameters["W" + str(layer)] = np.random.rand(self.neuron_per_layer_list[layer], self.neuron_per_layer_list[layer + 1]) # lignes == neurones actuels, col == neurones proch couche.
-            self.parameters["b" + str(layer)] = np.random.rand(self.neuron_per_layer_list[layer + 1]) # lignes = neurones proch couche
+        param_dim = []
+        param_dim.append(self.training_data_array.shape[1])
+        param_dim += self.neuron_per_layer_list
+        self.parameters["W0"] = np.random.rand(param_dim[0], param_dim[1])
+        self.parameters["b0"] = np.random.rand(param_dim[0], 1)
+        for layer in range(1, layers_nb + 1):
+            self.parameters["W" + str(layer)] = np.random.rand(param_dim[layer], param_dim[layer - 1]) # lignes == neurones actuels, col == neurones proch couche.
+            self.parameters["b" + str(layer)] = np.random.rand(param_dim[layer], 1) # lignes = neurones proch couche
 
     def training(self):
         """
@@ -62,6 +73,8 @@ class Training():
             gradients = self.back_propagation(a)
             self.parameters_update(gradients)
         self.save_model()
+        plt.plot(l)
+        plt.show()
     
     def predict(self): # mettre dans programme de predictions.
         A = self.forward_propagation()
@@ -69,8 +82,8 @@ class Training():
     
     def forward_propagation(self):
         layers_nb = len(self.neuron_per_layer_list)
-        a = {"A0" : self.training_data_array}
-        for layer in range(1, layers_nb + 1):
+        a = {"A0" : self.training_data_array.T}
+        for layer in range(1, layers_nb):
             z = self.parameters['W' + str(layer)].dot(a['A' + str(layer - 1)]) + self.parameters['b' + str(layer)]
             a["A" + str(layer)] = self.sigmoid(z)
         return a
@@ -79,18 +92,23 @@ class Training():
         z = np.clip(z, -500, 500)
         return (1 / (1 + np.exp(-z)))
 
-    def log_loss(self, A):
+    def log_loss(self, a):
+        """
+        Takes the activation of the output layer and calculate the
+        error with the log loss/binary cross entropy function.
+        """
+        a = a["A" + str(len(self.neuron_per_layer_list) - 1)]
         epsilon = 1e-15
-        A = np.clip(A, a_min= epsilon, a_max = 1-epsilon)
+        a = np.clip(a, a_min= epsilon, a_max = 1-epsilon)
         y = self.training_real_values
         m = len(y)
-        l = -(1/m) * np.sum(y * np.log(A) + (1 - y) * np.log(1 - A))
+        l = -(1/m) * np.sum(y * np.log(a) + (1 - y) * np.log(1 - a))
         return l
     
     def back_propagation(self, model_predictions):
-        layer_count = len(self.parameters) // 2
-        dZ = model_predictions["A" + str(layer_count)] - self.training_real_values
-        m = self.training_real_values.shape[1]
+        layer_count = len(self.neuron_per_layer_list)
+        dZ = model_predictions["A" + str(layer_count - 1)] - self.training_real_values
+        m = self.training_real_values.shape[0]
         gradients = {}
         for layer in reversed(range(1, layer_count + 1)):
             gradients["dW" + str(layer)] = 1 / m * np.dot(dZ, model_predictions["A" + str(layer-1)].T)
@@ -101,7 +119,7 @@ class Training():
     
     def parameters_update(self, gradients):
         layer_count = len(self.neuron_per_layer_list)
-        for layer in range(layer_count):
+        for layer in range(1, layer_count + 1):
             self.parameters["W" + str(layer)] = self.parameters["W" + str(layer)] - self.learning_rate * gradients["dW" + str(layer)]
             self.parameters["b" + str(layer)] = self.parameters["b" + str(layer)] - self.learning_rate * gradients["db" + str(layer)]
     
@@ -109,8 +127,9 @@ class Training():
         model_dict = {}
         model_dict["parameters"] = self.parameters
         model_dict["topology"] = self.neuron_per_layer_list
-        with open('model.json', 'w') as f:
-            json.dump(model_dict, f)
+        print (model_dict)
+        with open('model', 'w+') as f:
+            f.write(str(model_dict))
         
 
 t = Training()
