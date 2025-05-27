af987482e7ff2ab7cc689ec0f19a07102402280d
Theo Kempf <theokempf@protonmail.com>
2025-05-27 15:01:14 +0200

    softmax for output layer

--- Training.py
+++ Training.py
@@ -14,7 +14,7 @@ class Training():
         - poids biais.
         - nb episodes.
     """
-    def __init__(self, episodes_nb= 1000, neural_network_list= [24, 24, 24], learning_rate= 0.01):
+    def __init__(self, episodes_nb= 100, neural_network_list= [24, 24, 24], learning_rate= 0.01):
         self.epoch = episodes_nb
         self.nn_list = neural_network_list
         self.learning_rate = learning_rate
@@ -45,22 +45,25 @@ class Training():
         biais = liste avec element pour chaque couche. chaque element est tableau [nb neurones c, 1]
         Ajouter neurone de sortie.
         """
-        self.data_results[self.data_results == 'B'] = 0
-        self.data_results[self.data_results == 'M'] = 1
-        self.data_results = self.data_results.astype(np.float64)
+        new_array = np.zeros((self.data_results.shape[0], 2), dtype=np.float64)
+        new_array[self.data_results == 'M', 1] = 1
+        new_array[self.data_results == 'B', 0] = 1
+        self.data_results = new_array
         
+
         #normalize data
         self.data_measurements = (self.data_measurements - np.mean(self.data_measurements, axis=0)) / (np.std(self.data_measurements, axis=0) + 1e-8)
         
-        self.evaluation_data_results[self.evaluation_data_results == 'B'] = 0
-        self.evaluation_data_results[self.evaluation_data_results == 'M'] = 1
-        self.evaluation_data_results = self.evaluation_data_results.astype(np.float64)
+        new_array = np.zeros((self.evaluation_data_results.shape[0], 2), dtype=np.float64)
+        new_array[self.evaluation_data_results == 'M', 1] = 1
+        new_array[self.evaluation_data_results == 'B', 0] = 1
+        self.evaluation_data_results = new_array
         
         #normalize data
         self.evaluation_data_measurements = (self.evaluation_data_measurements - np.mean(self.evaluation_data_measurements, axis=0)) / (np.std(self.evaluation_data_measurements, axis=0) + 1e-8)
         
         input_layer_size = self.data_measurements.shape[1]
-        self.nn_list = [input_layer_size] + self.nn_list + [1]
+        self.nn_list = [input_layer_size] + self.nn_list + [2]
         np.random.seed(2)
         for layer in range(1, len(self.nn_list)):
             self.weights.append(np.random.rand(self.nn_list[layer], self.nn_list[layer-1]))
@@ -82,7 +85,7 @@ class Training():
         last_layer = len(self.nn_list) - 2 # -2 car compte pas la couche dentree et doit prendre l'index (qui commence a 0)
         z_layer = self.weights[last_layer].dot(activation[last_layer]) + self.biases[last_layer]
         z.append(z_layer)
-        activation.append(self.sigmoid(z_layer))
+        activation.append(self.softmax(z_layer))
         return activation
 
 
@@ -93,7 +96,7 @@ class Training():
         """
         dw = []
         db = []
-        y_true = self.data_results.reshape(1, -1)
+        y_true = self.data_results.T
         dz = activation[-1] - y_true
         layers = len(self.nn_list) - 1
         m = y_true.shape[1]
@@ -114,30 +117,31 @@ class Training():
             self.weights[layer] = self.weights[layer] - (self.learning_rate * dw[layer])
             self.biases[layer] = self.biases[layer] - (self.learning_rate * db[layer])
 
-    def sigmoid(self, z):
+    def softmax(self, z):
         """
         Softmax is used for the output layer.
         """
         z = np.clip(z, -500, 500)
-        return 1 / (1 + np.exp(-z))
+        exp_z = np.exp(z - np.max(z, axis=0, keepdims=True))
+        return exp_z / np.sum(exp_z, axis=0, keepdims=True)
 
     def relu(self, z):
         """
         relu formula is used for every layer except the output layer.
         """
-        #z = np.clip(z, -200, 200)
         return np.maximum(0, z)
     
     def log_loss(self, predictions, y_true):
-        y_true = y_true.reshape(1, -1)
+        y_true = y_true.T
         predictions = np.clip(predictions, 1e-15, 1 - 1e-15)
-        loss = -np.mean(y_true * np.log(predictions) + (1 - y_true) * np.log(1 - predictions))
+        loss = -np.mean(y_true * np.log(predictions))
         return loss
     
     def accuracy(self, predictions, true_results):
-        y_true = true_results.reshape(1, -1)
-        predicted_classes = (predictions > 0.5).astype(int)
-        accuracy = np.mean(predicted_classes == y_true)
+        y_true = true_results.T
+        predicted_classes = np.argmax(predictions, axis=0)
+        true_classes = np.argmax(y_true, axis=0)
+        accuracy = np.mean(predicted_classes == true_classes)
         return accuracy
     
     def evaluate(self, epoch=-1):
@@ -147,18 +151,18 @@ class Training():
         """
         if epoch ==-1:
             epoch = self.epoch
-        eval_activation = self.ev2(self.evaluation_data_measurements.T)
-        train_activation = self.ev2(self.data_measurements.T)
-        print(f"Epoch: {epoch}/{self.epoch}   Evaluation accuracy: {self.accuracy(eval_activation, self.evaluation_data_results)}   Evaluation loss: {self.log_loss(eval_activation.T, self.evaluation_data_results)}   Training accuracy {self.accuracy(train_activation, self.data_results)}   Training loss: {self.log_loss(train_activation.T, self.data_results)}")
+        eval_activation = self.eval_forward_prop(self.evaluation_data_measurements.T)
+        train_activation = self.eval_forward_prop(self.data_measurements.T)
+        print(f"Epoch: {epoch}/{self.epoch}   Evaluation accuracy: {self.accuracy(eval_activation, self.evaluation_data_results)}   Evaluation loss: {self.log_loss(eval_activation, self.evaluation_data_results)}   Training accuracy {self.accuracy(train_activation, self.data_results)}   Training loss: {self.log_loss(train_activation, self.data_results)}")
 
-    def ev2(self, data):
+    def eval_forward_prop(self, data):
         activation = data
         for layer in range(len(self.nn_list) - 2):
             z = self.weights[layer].dot(activation) + self.biases[layer]
             activation = self.relu(z)
         last_layer = len(self.nn_list) - 2 # -2 car compte pas la couche dentree et doit prendre l'index (qui commence a 0)
         z = self.weights[last_layer].dot(activation) + self.biases[last_layer]
-        activation = self.sigmoid(z)
+        activation = self.softmax(z)
         return activation
 
 t = Training()
